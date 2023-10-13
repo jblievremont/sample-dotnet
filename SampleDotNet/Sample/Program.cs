240bf01d657db25f15b84d16259a9f0b69ccb9da
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2023-10-13 16:18:25 +0200

    Add initial sample code

---
+++ SampleDotNet/Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (C) 2023 AsteroMitH
+ */
+
+namespace Sample
+{
+    public class SampleMain
+    {
+        private const string IpAddress = "12.34.56.78";
+
+        private SampleMain()
+        {
+            // Empty
+        }
+
+        public static void Main()
+        {
+            // TODO Add more stuff here
+            Console.WriteLine("Hey there! We'll contact " + IpAddress);
+        }
+    }
+}
